f359bbe8ddb4af3696ff3bba5701b3a8593e669c
Mucahit <mucahittceylann@hotmail.com>
2022-01-21 20:42:47 +0300

    redux pages

---
+++ src/appState/movies/constants.ts
@@ -0,0 +1,10 @@
+export const GET_POPULAR_MOVIES = '@MOVIE_GET_POPULAR_MOVIES';
+export const SET_POPULAR_MOVIES = '@MOVIE_SET_POPULAR_MOVIES';
+
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_OUT = 'SIGN_OUT';
+
+export const initialState = {
+  movies: [],
+};
