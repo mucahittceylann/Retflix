4dcd2508fffebc796cd4ab4fcafb3079359d386f
Mucahit <mucahittceylann@hotmail.com>
2022-01-31 22:28:34 +0300

    added flat list

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {FlatList} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {DbView} from '../../components';
+import {useDispatch, useSelector} from 'react-redux';
+import {getPopularMoviesAction} from '../../appState/movies/action';
+import {moviesSelector} from '../../appState/movies/selectors';
+import {DbText, DbView} from '../../components';
 import styles from './styles';
 
 const HomePage = () => {
+  const movies = useSelector(moviesSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPopularMoviesAction());
+  }, [dispatch]);
   return (
     <KeyboardAwareScrollView enableOnAndroid style={styles.scrollView}>
-      <DbView style={styles.container}></DbView>
+      <DbView style={styles.container}>
+        <FlatList
+          keyExtractor={item => item.title}
+          data={movies}
+          renderItem={({item}) => (
+            <DbText style={{color: 'white'}}>{item.title}</DbText>
+          )}
+        />
+      </DbView>
     </KeyboardAwareScrollView>
   );
 };
