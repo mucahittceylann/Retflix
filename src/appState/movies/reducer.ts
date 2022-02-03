72b6be6e1831ee6b396d01c4231077aa4e60bd59
Suleyman Kocak <slymnkocakk@gmail.com>
2022-02-03 17:44:54 +0300

    minor fixes

--- src/appState/movies/reducer.ts
+++ src/appState/movies/reducer.ts
@@ -11,8 +11,9 @@ import update from 'immutability-helper';
 export const movieReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case SET_POPULAR_MOVIES:
-      state.popularMovies = action.movies;
-      return {...state};
+      return update(state, {
+        popularMovies: {$set: action.movies},
+      });
     case SET_ACTIVE_MOVIE: {
       return update(state, {
         activeMovie: {$set: action.movie},
