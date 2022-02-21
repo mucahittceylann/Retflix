2085de78dbd0545c87cb358cc56e1ae75117e0b9
Mucahit <mucahittceylann@hotmail.com>
2022-02-22 01:20:39 +0300

    minor fixes

--- src/screens/myList/index.tsx
+++ src/screens/myList/index.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {DbText, DbView} from '../../components';
-import styles from './styles';
 import {useSelector} from 'react-redux';
 import {favoritesSelector} from '../../appState/movies/selectors';
-import MovieList from '../../components/MovieList';
+import TripleMovieList from '../../components/TripleMovieList';
 
 const MyListPage = () => {
   const favorites = useSelector(favoritesSelector);
 
-  return (
-    <KeyboardAwareScrollView enableOnAndroid style={styles.scrollView}>
-      <DbView style={styles.container}>
-        <DbText style={styles.whiteBoldText}>Favorites</DbText>
-        <MovieList data={favorites} imageStyle={styles.image} />
-      </DbView>
-    </KeyboardAwareScrollView>
-  );
+  return <TripleMovieList data={favorites} />;
 };
 
 export default MyListPage;
