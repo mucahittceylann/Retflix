fb95de14164e283472ae7746382975b993bb1e31
Mucahit <mucahittceylann@hotmail.com>
2022-02-22 01:20:21 +0300

    added handleLiked

--- src/screens/detail/ActionsView/index.tsx
+++ src/screens/detail/ActionsView/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Octicons from 'react-native-vector-icons/Octicons';
@@ -8,19 +8,22 @@ import {useDispatch, useSelector} from 'react-redux';
 import {
   activeMovieSelector,
   existInFavoritesSelector,
+  existInLikedSelector,
 } from '../../../appState/movies/selectors';
-import firestore from '@react-native-firebase/firestore';
+import firestore, {firebase} from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import {Movie} from '../../../shared/types/movie';
 import {
   deleteFromFavoritesAction,
+  deleteFromLikedAction,
   saveToFavoritesAction,
+  saveToLikedAction,
 } from '../../../appState/movies/action';
 
 const ActionsView = () => {
-  const [liked, setLiked] = useState(false);
   const activeMovie: Movie = useSelector(activeMovieSelector)!;
   const existInFavorites: boolean = useSelector(existInFavoritesSelector);
+  const existInLiked: boolean = useSelector(existInLikedSelector);
   const dispatch = useDispatch();
 
   const handleAddlist = async () => {
@@ -55,8 +58,36 @@ const ActionsView = () => {
       });
   };
 
-  const handleRating = () => {
-    setLiked(!liked);
+  const handleLiked = async () => {
+    if (existInLiked) {
+      await firestore()
+        .collection('likedMovies')
+        .where('id', '==', activeMovie.id)
+        .where('ownerEmail', '==', firebase.auth().currentUser?.email)
+        .get()
+        .then(res => {
+          const documentId = res.docs[0].id;
+          firestore()
+            .collection('likedMovies')
+            .doc(documentId)
+            .delete()
+            .then(() => {
+              dispatch(deleteFromLikedAction(activeMovie));
+            });
+        });
+      return;
+    }
+    const likedMovie: Movie = {
+      ...activeMovie,
+      ownerEmail: auth().currentUser!.email!,
+    };
+
+    await firestore()
+      .collection('likedMovies')
+      .add(likedMovie)
+      .then(() => {
+        dispatch(saveToLikedAction(likedMovie));
+      });
   };
 
   return (
@@ -73,9 +104,9 @@ const ActionsView = () => {
       </DbView>
 
       <DbView style={styles.iconView2}>
-        <TouchableOpacity onPress={handleRating}>
+        <TouchableOpacity onPress={handleLiked}>
           <FontAwesome
-            name={liked ? 'thumbs-o-up' : 'thumbs-o-down'}
+            name={existInLiked ? 'thumbs-up' : 'thumbs-o-up'}
             color="white"
             size={24}
           />
